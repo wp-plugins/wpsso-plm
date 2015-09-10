36138c8fb33f65ceb4a1e0abc55c17277a4a8c94
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-10 16:00:34 +0000

    v1.3.5
    git-svn-id: https://plugins.svn.wordpress.org/wpsso-plm/tags/1.3.5@1242565 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/config.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl.txt
+ * Copyright 2014-2015 - Jean-Sebastien Morisset - http://wpsso.com/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) 
+	die( 'These aren\'t the droids you\'re looking for...' );
+
+if ( ! class_exists( 'WpssoPlmConfig' ) ) {
+
+	class WpssoPlmConfig {
+
+		public static $cf = array(
+			'plugin' => array(
+				'wpssoplm' => array(
+					'version' => '1.3.5',	// plugin version
+					'short' => 'WPSSO PLM',
+					'name' => 'WPSSO Place and Location Meta (WPSSO PLM)',
+					'desc' => 'WPSSO extension to provide Facebook / Open Graph "Location" and Pinterest "Place" Rich Pin meta tags.',
+					'slug' => 'wpsso-plm',
+					'base' => 'wpsso-plm/wpsso-plm.php',
+					'update_auth' => 'tid',
+					'img' => array(
+						'icon_small' => 'images/icon-128x128.png',
+						'icon_medium' => 'images/icon-256x256.png',
+					),
+					'url' => array(
+						// wordpress
+						'download' => 'https://wordpress.org/plugins/wpsso-plm/',
+						'review' => 'https://wordpress.org/support/view/plugin-reviews/wpsso-plm?filter=5&rate=5#postform',
+						'readme' => 'https://plugins.svn.wordpress.org/wpsso-plm/trunk/readme.txt',
+						'wp_support' => 'https://wordpress.org/support/plugin/wpsso-plm',
+						// surniaulula
+						'update' => 'http://wpsso.com/extend/plugins/wpsso-plm/update/',
+						'purchase' => 'http://wpsso.com/extend/plugins/wpsso-plm/',
+						'changelog' => 'http://wpsso.com/extend/plugins/wpsso-plm/changelog/',
+						'codex' => 'http://wpsso.com/codex/plugins/wpsso-plm/',
+						'faq' => 'http://wpsso.com/codex/plugins/wpsso-plm/faq/',
+						'notes' => '',
+						'feed' => 'http://wpsso.com/category/application/wordpress/wp-plugins/wpsso-plm/feed/',
+						'pro_support' => 'http://wpsso-plm.support.wpsso.com/',
+					),
+					'lib' => array(
+						'submenu' => array (
+							'wpssoplm-separator-0' => 'PLM Extension',
+							'plm-general' => 'Place and Location Meta',
+						),
+						'gpl' => array(
+							'admin' => array(
+								'plm-general' => 'Place and Location Meta',
+							),
+						),
+						'pro' => array(
+							'admin' => array(
+								'plm-general' => 'Place and Location Meta',
+							),
+							'head' => array(
+								'place-meta' => 'Place Meta Tags',
+							),
+						),
+					),
+					'cp' => array(
+					)
+				),
+			),
+		);
+
+		public static function set_constants( $plugin_filepath ) { 
+			$slug = self::$cf['plugin']['wpssoplm']['slug'];
+
+			define( 'WPSSOPLM_FILEPATH', $plugin_filepath );						
+			define( 'WPSSOPLM_PLUGINDIR', trailingslashit( realpath( dirname( $plugin_filepath ) ) ) );
+			define( 'WPSSOPLM_PLUGINBASE', plugin_basename( $plugin_filepath ) );
+			define( 'WPSSOPLM_TEXTDOM', $slug );
+			define( 'WPSSOPLM_URLPATH', trailingslashit( plugins_url( '', $plugin_filepath ) ) );
+		}
+
+		public static function require_libs( $plugin_filepath ) {
+			require_once( WPSSOPLM_PLUGINDIR.'lib/register.php' );
+			add_filter( 'wpssoplm_load_lib', array( 'WpssoPlmConfig', 'load_lib' ), 10, 3 );
+		}
+
+		// gpl / pro library loader
+		public static function load_lib( $ret = false, $filespec = '', $classname = '' ) {
+			if ( $ret === false && ! empty( $filespec ) ) {
+				$filepath = WPSSOPLM_PLUGINDIR.'lib/'.$filespec.'.php';
+				if ( file_exists( $filepath ) ) {
+					require_once( $filepath );
+					if ( empty( $classname ) )
+						return 'wpssoplm'.str_replace( array( '/', '-' ), '', $filespec );
+					else return $classname;
+				}
+			}
+			return $ret;
+		}
+	}
+}
+
+?>
